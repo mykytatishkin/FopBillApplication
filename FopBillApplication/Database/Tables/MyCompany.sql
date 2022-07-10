705b8d630a75ca8e2faaf3b7b6f37bc4778245ff
GTeasera <87891550+GTeasera@users.noreply.github.com>
2022-07-10 11:34:33 +0300

    0.0.2
    I add database, SQL Server. Also, I make 1 table

---
+++ FopBillApplication/Database/Tables/MyCompany.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[MyCompany]
+(
+	[Id] INT NOT NULL, 
+    [Name] NCHAR(255) NOT NULL, 
+    [Code] NCHAR(50) NOT NULL, 
+    [Address] NCHAR(255) NOT NULL
+    CONSTRAINT PK_MyCompany_Id primary key (Id)
+);
